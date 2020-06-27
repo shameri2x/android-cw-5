9524c64514023daa3383b66fb2445113fa0b9b98
46- احمد الشمري <shameri2x@gmail.com>
2020-06-27 19:13:42 +0300

    not complete until now...

---
+++ cw5/app/src/main/java/com/example/cw5/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.cw5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        EditText name = findViewById(R.id.editText);
+        Button next = findViewById(R.id.button);
+        next.setText();
+
+
+    }
+}
